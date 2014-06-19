9806971f2709cba6b4b3ee0ea28659f1ca2a739e
Raphael Ivan <raphaivan@gmail.com>
2014-06-19 13:41:14 -0300

    Change host to http://worldcup.sfg.io/

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,47 +3,49 @@
 ;(function () {
   var
       request = require('request')
-    , url  = 'http://footballdb.herokuapp.com/api/v1/event/world.2014'
+    , url  = 'http://worldcup.sfg.io/'
     , Api = {};
 
 
   Api.teams = function (callback) {
     request.get(url + '/teams', function (error, response, body) {
-      var body = JSON.parse(response.body);
-      callback(body.teams); //{ key: '', title: '', code: '' }
+      var json = JSON.parse(body);
+      callback(json);
+      /*
+        { country: 'Croatia',
+         alternate_name: null,
+         fifa_code: 'CRO',
+         group_id: 1
+        }
+      */
+
     });
   };
 
 
-  Api.rounds = function (round, callback) {
-    if (round) {
-      request.get(url + '/round/'+ round, function (error, response, body) {
-        var body = JSON.parse(response.body);
-        callback(body);
-        /*
-          { team1_key: 'bra',
-            team1_title: 'Brazil',
-            team1_code: 'BRA',
-            team2_key: 'cro',
-            team2_title: 'Croatia',
-            team2_code: 'CRO',
-            play_at: '2014/06/12',
-            score1: 3,
-            score2: 1,
-            score1ot: null,
-            score2ot: null,
-            score1p: null,
-            score2p: null
-          }
-        */
-      });
-    } else {
-      request.get(url + '/rounds', function (error, response, body) {
-        var body = JSON.parse(response.body);
-        callback(body.rounds); //{ pos: 20, title: 'Final', start_at: '2014/07/13', end_at: '2014/07/13' }
-      });
-    }
+  Api.matches = function (callback) {
+    request.get(url + '/matches/', function (error, response, body) {
+      var json = JSON.parse(body);
+      callback(json);
+      /*
+        {
+          match_number: 1,
+          location: 'Arena Corinthians',
+          datetime: '2014-06-12T17:00:00.000-03:00',
+          status: 'completed',
+          home_team:
+           { country: 'Brazil',
+             code: 'BRA',
+             goals: 3 },
+          away_team:
+           { country: 'Croatia',
+             code: 'CRO',
+             goals: 1 },
+          winner: 'Brazil'
+        }
+      */
+    });
   }
 
-  exports.WordCup = Api;
+  exports.WorldCup = Api;
 })();
